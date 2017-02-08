218c65dd53737cbefc04a0f08684b891d3929163
Ck1 <cadekynaston@gmail.com>
2017-02-07 23:11:56 -0700

    added more stuff

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'This is the login page.' });
+  res.render('chat', { title: 'This is a test chat page.' });
 });
 
 module.exports = router;
